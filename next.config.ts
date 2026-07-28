7acf6fbdbdab0fa67ae13dd20ae4cecd5fdf3c02
Rio <roidelossantos22@gmail.com>
2026-07-28 15:15:34 -0500

    Revert next.config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // For Docker/deployment
-  // Enable static generation where possible
-  experimental: {
-    // Add any experimental features you need
-  },
 };
 
 export default nextConfig;
